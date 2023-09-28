bf45750fae4791c3c03715a1a860abcd33ca18f0
J Meyer <124314330+BinkSan3@users.noreply.github.com>
2023-09-28 13:17:46 +0100

    Funtioning but about to add things that could potentially break it

--- src/App.js
+++ src/App.js
@@ -1,23 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [list, setList] = useState(["thing1", "thing2"]);
+  const [inputText, setInputText] = useState("");
+  const [archive, setArchive] = useState(["thing x"]);
+  const [archiveVisibility, setArchiveVisibility] = useState(false);
+  const [editing, setEditing] = useState(false);
+
+  const toggleArchive = () => {
+    setArchiveVisibility(!archiveVisibility);
+  };
+  const toggleEdit = (thisEdit) => {
+    setEditing(!editing);
+  };
+
+  const addHandler = (event) => {
+    event.preventDefault();
+
+    let storedList = [...list];
+    storedList.push(inputText);
+    setList(storedList);
+
+    setInputText("");
+  };
+
+  const removeHandler = (position) => {
+    let storedList = [...list];
+    let storedArchive = [...archive];
+    storedArchive.push(storedList.splice(position, 1));
+    setList(storedList);
+    setArchive(storedArchive);
+  };
+  const deletePerm = (position) => {
+    let storedArchive = [...archive];
+    storedArchive.splice(position, 1);
+    setArchive(storedArchive);
+  };
+
+  const changeHandler = (event) => {
+    setInputText(event.target.value);
+  };
+
+  // const editHandler = () => {
+  //   setEditing(true);
+  // };
+  // const saveHandler = () => {
+  //   setEditing(false);
+  // };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="toDoList">
+        <form onSubmit={addHandler}>
+          <input type="text" value={inputText} onChange={changeHandler}></input>
+          <button type="submit">+</button>
+        </form>
+        {list.map((toDo, index) => {
+          return (
+            <p key={index}>
+              {toDo}
+              <button onClick={() => toggleEdit(index)}>
+                {editing ? "Save" : "Edit"}
+              </button>
+              <button onClick={() => removeHandler(index)}>-</button>
+            </p>
+          );
+        })}
+      </div>
+      <button onClick={toggleArchive}>
+        {archiveVisibility ? "Hide Archive" : "Show Archive"}
+      </button>
+      {archiveVisibility && (
+        <div id="archive">
+          <h2>ARCHIVED</h2>
+          {archive.map((toDo, index) => {
+            return (
+              <div key={index}>
+                <p>{toDo}</p>
+                <button></button>
+                <button onClick={() => deletePerm(index)}>-</button>
+              </div>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 }
